8cc23ac0632fe6bc386fc51fa9623735259c509b
Bilal Hodzic <hodzic.bilal@hotmail.com>
2021-10-28 18:21:42 +0200

    Updates

---
+++ precache-manifest.a3513bf335c1916053119b385eb8cd8d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32214571b2ac584589d5d2f7feaf0aad",
+    "url": "/flawlessAI/index.html"
+  },
+  {
+    "revision": "95b552d7f84e0152dd15",
+    "url": "/flawlessAI/static/css/main.fda1fe51.chunk.css"
+  },
+  {
+    "revision": "e2e0211670a9a5954ed8",
+    "url": "/flawlessAI/static/js/2.5e0988be.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/flawlessAI/static/js/2.5e0988be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95b552d7f84e0152dd15",
+    "url": "/flawlessAI/static/js/main.a41509a0.chunk.js"
+  },
+  {
+    "revision": "d573ab8d993827442253",
+    "url": "/flawlessAI/static/js/runtime-main.034da351.js"
+  },
+  {
+    "revision": "d127c5ef798bd4c4613fc193169bdb5d",
+    "url": "/flawlessAI/static/media/LogoWhite.d127c5ef.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/flawlessAI/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "1ee2ee63f198b8ab1dde99aff3a45336",
+    "url": "/flawlessAI/static/media/demo-placeholder.1ee2ee63.jpg"
+  },
+  {
+    "revision": "ac0a3c3996b0a786c1bb2e63594bedc3",
+    "url": "/flawlessAI/static/media/feature-analytics.ac0a3c39.png"
+  },
+  {
+    "revision": "1a10fe5987a8715b48f4739f18778de2",
+    "url": "/flawlessAI/static/media/feature-contract.1a10fe59.png"
+  },
+  {
+    "revision": "82fb49b0fdca8c34212caf616f257d1c",
+    "url": "/flawlessAI/static/media/feature-document.82fb49b0.png"
+  },
+  {
+    "revision": "8cd684be6d62894a37ad40a8b149ace9",
+    "url": "/flawlessAI/static/media/feature-managment.8cd684be.png"
+  },
+  {
+    "revision": "ab2f2768a7034acb1d98dbee9f03182c",
+    "url": "/flawlessAI/static/media/feature-robot.ab2f2768.png"
+  },
+  {
+    "revision": "0f16f2fccd8da7a704fec09ed1e2ace5",
+    "url": "/flawlessAI/static/media/features-deals.0f16f2fc.png"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/flawlessAI/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/flawlessAI/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/flawlessAI/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/flawlessAI/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/flawlessAI/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "58c2edd43e03faefd708bcbc5f0c939d",
+    "url": "/flawlessAI/static/media/team-bilal.58c2edd4.png"
+  },
+  {
+    "revision": "0ce76bc220e3a2a0c44051527617c5ce",
+    "url": "/flawlessAI/static/media/team-dado.0ce76bc2.png"
+  },
+  {
+    "revision": "8eb5599810a551941834996427d8dcc4",
+    "url": "/flawlessAI/static/media/team-enid.8eb55998.png"
+  },
+  {
+    "revision": "b7fa737cbd60fd32475eb44dcec1df59",
+    "url": "/flawlessAI/static/media/team-faruk.b7fa737c.png"
+  },
+  {
+    "revision": "3757ece09cb165f95ab31f257b873b7e",
+    "url": "/flawlessAI/static/media/team-sinisa.3757ece0.png"
+  }
+]);
